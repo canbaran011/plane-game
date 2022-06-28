21438ada2808a1fddd658b832d02753a1aeb55e2
Can Baran Gündüz <canbaran011@gmail.com>
2022-06-28 23:48:29 +0300

    scorer

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text playerScoreText;
+
     private int _playerScore;
-    private int _otherScore;
 
     public void PlayerScores(){
         _playerScore++;
+        this.playerScoreText.text = _playerScore.ToString();
         Debug.Log(_playerScore);
     }
     
-    public void OtherScores(){
-        _otherScore++;
-        Debug.Log(_otherScore);
-    }
+
 }
--- Assets/ScoringZone.cs
+++ Assets/ScoringZone.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ScoringZone : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision){
+    public EventTrigger.TriggerEvent scoreTrigger;
+
+    public void OnCollisionEnter(Collision collision){
+
+
+        Debug.Log("There is a collision");
+        BaseEventData eventData = new BaseEventData(EventSystem.current);
+        this.scoreTrigger.Invoke(eventData);
+
+ 
         
     }
+    public void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("There is a trigger");
+        BaseEventData eventData = new BaseEventData(EventSystem.current);
+        this.scoreTrigger.Invoke(eventData);
+    }
+
+    
 
 }
